c7c0401728afff091c00a721354a8f13c055377d
gollanai <aidangollan@icloud.com>
2024-05-14 21:30:30 -0400

    improvements to project page

--- src/app/projects/exampleProjects.tsx
+++ src/app/projects/exampleProjects.tsx
@@ -4,83 +4,134 @@ export const exampleProjects = [
         name: "Notello",
         description: "An AI powered stufy helper",
         image: "/def-project.png",
-        logo: "Symbol-Brand-Colors.png"
+        logo: "Symbol-Brand-Colors.png",
+        skills: [
+            "Next.js",
+            "Tailwind CSS",
+            "TypeScript",
+            "Vercel",
+            "React",
+            "Supabase",
+        ]
     },
     {
         id: "2",
         name: "Quarry",
         description: "A Tiktok Clip Generator",
         image: "/def-project.png",
-        logo: "Symbol-Brand-Colors.png"
+        logo: "Symbol-Brand-Colors.png",
+        skills: [
+            "Next.js",
+            "Tailwind CSS",
+            "TypeScript",
+            "Vercel",
+            "React",
+            "Supabase",
+        ]
     },
     {
         id: "3",
         name: "RecycleMe",
         description: "A recycling quality detection app",
         image: "/def-project.png",
-        logo: "Symbol-Brand-Colors.png"
+        logo: "Symbol-Brand-Colors.png",
+        skills: [
+            "Next.js",
+            "Tailwind CSS",
+            "TypeScript",
+            "Vercel",
+            "React",
+            "Supabase",
+        ]
     },
     {
         id: "1",
         name: "Notello",
         description: "An AI powered stufy helper",
         image: "/def-project.png",
-        logo: "Symbol-Brand-Colors.png"
+        logo: "Symbol-Brand-Colors.png",
+        skills: [
+            "Next.js",
+            "Tailwind CSS",
+            "TypeScript",
+            "Vercel",
+            "React",
+            "Supabase",
+        ]
     },
     {
         id: "2",
         name: "Quarry",
         description: "A Tiktok Clip Generator",
         image: "/def-project.png",
-        logo: "Symbol-Brand-Colors.png"
+        logo: "Symbol-Brand-Colors.png",
+        skills: [
+            "Next.js",
+            "Tailwind CSS",
+            "TypeScript",
+            "Vercel",
+            "React",
+            "Supabase",
+        ]
     },
     {
         id: "3",
         name: "RecycleMe",
         description: "A recycling quality detection app",
         image: "/def-project.png",
-        logo: "Symbol-Brand-Colors.png"
+        logo: "Symbol-Brand-Colors.png",
+        skills: [
+            "Next.js",
+            "Tailwind CSS",
+            "TypeScript",
+            "Vercel",
+            "React",
+            "Supabase",
+        ]
     },
     {
         id: "1",
         name: "Notello",
         description: "An AI powered stufy helper",
         image: "/def-project.png",
-        logo: "Symbol-Brand-Colors.png"
+        logo: "Symbol-Brand-Colors.png",
+        skills: [
+            "Next.js",
+            "Tailwind CSS",
+            "TypeScript",
+            "Vercel",
+            "React",
+            "Supabase",
+        ]
     },
     {
         id: "2",
         name: "Quarry",
         description: "A Tiktok Clip Generator",
         image: "/def-project.png",
-        logo: "Symbol-Brand-Colors.png"
+        logo: "Symbol-Brand-Colors.png",
+        skills: [
+            "Next.js",
+            "Tailwind CSS",
+            "TypeScript",
+            "Vercel",
+            "React",
+            "Supabase",
+        ]
     },
     {
         id: "3",
         name: "RecycleMe",
         description: "A recycling quality detection app",
         image: "/def-project.png",
-        logo: "Symbol-Brand-Colors.png"
-    },
-    {
-        id: "1",
-        name: "Notello",
-        description: "An AI powered stufy helper",
-        image: "/def-project.png",
-        logo: "Symbol-Brand-Colors.png"
-    },
-    {
-        id: "2",
-        name: "Quarry",
-        description: "A Tiktok Clip Generator",
-        image: "/def-project.png",
-        logo: "Symbol-Brand-Colors.png"
-    },
-    {
-        id: "3",
-        name: "RecycleMe",
-        description: "A recycling quality detection app",
-        image: "/def-project.png",
-        logo: "Symbol-Brand-Colors.png"
+        logo: "Symbol-Brand-Colors.png",
+        skills: [
+            "Next.js",
+            "Tailwind CSS",
+            "TypeScript",
+            "Vercel",
+            "React",
+            "Supabase",
+        ]
     },
 ]
--- src/app/projects/projectCard.tsx
+++ src/app/projects/projectCard.tsx
@@ -1,32 +1,45 @@
-import { Button, Card, CardFooter, CardHeader } from "@nextui-org/react";
+import { Button, Card, CardFooter, CardHeader, Chip } from "@nextui-org/react";
 import { Image } from "@nextui-org/react";
 import Link from "next/link";
 
 export async function ProjectCard({ project } : { project: any }) {
     return (
-        <Card isFooterBlurred className="w-full h-[300px] transform transition duration-500 hover:-translate-y-2">
-        <CardHeader className="absolute z-10 top-1 flex-col items-start">
-          <p className="text-tiny text-white/60 uppercase font-bold">{project.name}</p>
-        </CardHeader>
-        <Image
-          removeWrapper
-          alt="Relaxing app background"
-          className="z-0 w-full h-full object-cover"
-          src={project.image}
-        />
-        <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-          <div className="flex flex-grow gap-2 items-center">
+        <Card isFooterBlurred className="w-full h-[300px] transform transition duration-500 hover:-translate-y-1">
+            <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                <p className="text-tiny text-white/60 uppercase font-bold">{project.name}</p>
+                <p className="text-tiny text-white/60 font-bold">{project.description}</p>
+            </CardHeader>
             <Image
-              alt="Breathing app icon"
-              className="rounded-full w-10 h-11 bg-black"
-              src={project.logo}
+                removeWrapper
+                alt="Relaxing app background"
+                className="z-0 w-full h-full object-cover"
+                src={project.image}
             />
-            <div className="flex flex-col">
-              <p className="text-tiny text-white/60">{project.description}</p>
-            </div>
-          </div>
-          <Button as={Link} href={`projects/${project.id}`} radius="full" size="sm">Learn More</Button>
-        </CardFooter>
-      </Card>
+            <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
+                <div className="flex gap-2 items-center w-full">
+                    <div className="flex-shrink-0 w-10 h-11">
+                        <Image
+                            alt="Breathing app icon"
+                            className="rounded-full w-full h-full bg-black"
+                            src={project.logo}
+                        />
+                    </div>
+                    <div className="flex flex-grow gap-2 items-center flex-wrap">
+                        {project.skills.map((skill: string) => (
+                            <Chip 
+                                key={skill} 
+                                className="text-white/60 bg-black/40 rounded-full"
+                                size="sm"
+                            >
+                                {skill}
+                            </Chip>
+                        ))}
+                    </div>
+                    <Button as={Link} href={`projects/${project.id}`} radius="full" size="sm" className="flex-shrink-0">
+                        Learn More
+                    </Button>
+                </div>
+            </CardFooter>
+        </Card>
     )
 }
