b3994508fb6a2435d7ba0f6df688ceb3de53a467
MAlshaik <alshaik8@msu.edu>
2024-05-23 00:51:35 +0300

    salam

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -27,7 +27,7 @@ export function Footer() {
                     </ul>
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-                <span classNameName="block text-sm text-gray-500 sm:text-center dark:text-gray-400"><Link href="https://www.linkedin.com/company/imaginemsu/" class="hover:underline">Made by Imagine Software</Link> </span>
+                <span classNameName="block text-sm text-gray-500 sm:text-center dark:text-gray-400"><Link href="https://www.linkedin.com/company/imaginemsu/" class="hover:underline">Made in collaboration with Imagine Software</Link> </span>
             </div>
         </footer>
     );
